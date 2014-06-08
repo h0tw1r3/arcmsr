56da3e5b57ac3e6adeb4d890dd97834c4975fef6
Ching <ching2048@areca.com.tw>
2014-06-07 23:33:45 -0500

    This is a new patch v1.2 18/16 after patch v1.2 17/16. To prevent flushing entire workqueue, replace flush_scheduled_work by flush_work.
    Signed-off-by: Ching<ching2048@areca.com.tw>

--- arcmsr_hba.c
+++ arcmsr_hba.c
@@ -845,7 +845,7 @@ static int arcmsr_suspend(struct pci_dev *pdev, pm_message_t state)
 	intmask_org = arcmsr_disable_outbound_ints(acb);
 	arcmsr_free_irq(pdev, acb);
 	del_timer_sync(&acb->eternal_timer);
-	flush_scheduled_work();
+	flush_work(&acb->arcmsr_do_message_isr_bh);
 	arcmsr_stop_adapter_bgrb(acb);
 	arcmsr_flush_adapter_cache(acb);
 	arcmsr_enable_outbound_ints(acb, intmask_org);
